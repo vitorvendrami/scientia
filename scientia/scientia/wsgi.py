4f34cd94580846c72f8a6d271306f31fe55da550
vitor <vitor_vendrami@hotmail.com>
2021-08-08 10:48:46 -0300

    lint process

--- scientia/scientia/wsgi.py
+++ scientia/scientia/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scientia.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scientia.settings")
 
 application = get_wsgi_application()
