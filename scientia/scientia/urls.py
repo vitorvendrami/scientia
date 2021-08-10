c6a2c0c35cb08aae1367951563ff421b9765ca75
vitor <vitor_vendrami@hotmail.com>
2021-08-10 17:41:47 -0300

    update url

--- scientia/scientia/urls.py
+++ scientia/scientia/urls.py
@@ -18,12 +18,7 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Pastebin API')
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('network.urls')),
-    path('doc/', schema_view)
 ]
