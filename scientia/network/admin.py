97291d7960538b924f6a31e3dcb1d9d95c4a9701
vitor <vitor_vendrami@hotmail.com>
2021-08-10 16:15:46 -0300

    Registered model at admin

--- scientia/network/admin.py
+++ scientia/network/admin.py
@@ -1,3 +1,16 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import NetworkModel, GenerateEnvironment
+
+
+@admin.register(NetworkModel)
+class NetworkAdmin(admin.ModelAdmin):
+    list_display = ['user', 'uuid']
+    empty_value_display = "-empty-"
+
+
+@admin.register(GenerateEnvironment)
+class GenerateAdmin(admin.ModelAdmin):
+    list_display = ['uuid',]
+    empty_value_display = "-empty-"
